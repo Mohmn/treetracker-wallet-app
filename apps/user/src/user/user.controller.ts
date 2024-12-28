302c6de5fd9eb51425d82e577dd18bcdc6bf6c7f
Mohmn <naqashmohmin1@gmail.com>
2024-12-29 00:48:57 +0530

    feat: fire build

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -13,6 +13,6 @@ export class UserController {
 
   @Get('test')
   check(@Body() registerUserDto: RegisterUserDto) {
-    return 'tree growing!!!@dd#mdd@';
+    return 'tree growing!!!@dd#mdd@f';
   }
 }
