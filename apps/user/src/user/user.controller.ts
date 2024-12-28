5631ceb47a52ac1d21db66766412028f81529710
Mohmn <naqashmohmin1@gmail.com>
2024-12-29 01:02:45 +0530

    feat: add changes

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -13,6 +13,6 @@ export class UserController {
 
   @Get('test')
   check(@Body() registerUserDto: RegisterUserDto) {
-    return 'tree growing!!!@dd#mdd@f';
+    return 'tree growing!!!@dd#mdd@fs';
   }
 }
